5d218ea12c742820d58e65856738dbb0c1a8dc4a
Eugenio La Cava <eugeniolcv@gmail.com>
2025-07-28 08:58:59 +0200

    Auto-commit: chunk 1/2 - create_https_issue_direct.py (7 lines)
    This chunk includes:
    - Added module documentation and metadata
    - Added import statements and dependencies
    - Added import statements and dependencies

---
+++ q-learning-mfcs/create_https_issue_direct.py
@@ -0,0 +1,6 @@
+"""
+Direct GitLab API call to create HTTPS/SSL enhancement issue
+"""
+import sys
+import os
+from gitlab_issue_manager import GitLabIssueManager, IssueData, IssueType, IssueSeverity, IssueUrgency
